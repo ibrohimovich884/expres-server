d0d56227933f253649b633531790900e7dc1756a
Azimbek Alibekov <atalgat592@gmail.com>
2025-08-11 18:09:09 +0500

    sdfg

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Super servers");
+const express = require("express");
+
+const app = express();
+
+app.get("/health", (req, res) => res.end({ message: "OK!" }));
+
+console.log('hello');
+app.listen(3000, () => console.info('Server is running'));
